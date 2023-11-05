3be8535e99c3ed8431e476a4124dba8b83664a2d
Marcia Gameleira <gameleira.mag@hotmail.com>
2023-11-05 20:03:14 -0300

    .

---
+++ Cobol Completo do Basico ao Sistema com Telas - Udemy/2-Linguagem Cobol/PROG11.cbl
@@ -0,0 +1,27 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PROG11.
+      *-----------------------------------------------------------------
+      *== AUTOR: MARCIA GAMELEIRA
+      *== OBJETIVO: TRABALHANDO COM VETORES.
+      *== DATA: 05/11/2023
+      *== OBSERVACOES:
+      *-----------------------------------------------------------------
+
+      *=================================================================
+       ENVIRONMENT DIVISION.
+       CONFIGURATION   SECTION.
+       SPECIAL-NAMES.
+           DECIMAL-POINT IS COMMA.
+      *=================================================================
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       77 WRK-NOME         PIC X(30)       OCCURS 3 TIMES.
+
+      *=================================================================
+       PROCEDURE DIVISION.
+
+
+
+
+
+       END PROGRAM PROG11.
