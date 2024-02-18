51d32c4d967bdbb01737a1a668b00ef77e0f2220
Marcia Gameleira <gameleira.mag@hotmail.com>
2024-02-17 21:10:25 -0300

    .

--- C4-Livro Aprenda a Programar em Cobol/PROGEXEM.cbl
+++ C4-Livro Aprenda a Programar em Cobol/PROGEXEM.cbl
@@ -1,36 +1,65 @@
-       IDENTIFICATION  DIVISION.
-       PROGRAM-ID. PROGEXEM.
       ******************************************************************
-       ENVIRONMENT     DIVISION.
+       IDENTIFICATION  DIVISION.
+      ******************************************************************
+
+       PROGRAM-ID. PROGEXEM.
+
+      ******************************************************************
+       ENVIRONMENT DIVISION.
+      ******************************************************************
+
+      *----------------------------------------------------------------*
        CONFIGURATION   SECTION.
+      *----------------------------------------------------------------*
+
        SPECIAL-NAMES.
            DECIMAL-POINT IS COMMA.
+
+      *----------------------------------------------------------------*
        INPUT-OUTPUT    SECTION.
+      *----------------------------------------------------------------*
+
        FILE-CONTROL.
       * Neste parágrafo devemos definir cada um dos arquivos que o pro-
       * grama irá utilizar, de forma individualizada. Usa-se a cláusula
       * SELECT para cada arquivo que será usado pelo programa, tanto ar-
       * quivos de entrada como de saída.
       * Ex: SELECT <nome-arquivo> ASSIGN TO <disp-do-computador>.
-      * <nome-arquivo>: identifica o arquivo internamente dentro do pro-
-      *                 grama.
-      * <disp-do-computador>: identifica o nome físico do arquivo no am-
-      *                       biente operacio-nal. Se o programa vier a
-      *                       ser executado no mainframe, este nome deve
-      *                       estar associado aos comandos "JCL" na exe-
-      *                       cução do programa (cartão DD que identifi-
-      *                       ca o nome físico do arquivo associado com
-      *                       o nome lógico).
+      * <nome-arquivo>: nome lógico do arquivo. Identifica o arquivo in-
+      *                 ternamente dentro do programa.Se o programa vier
+      *                 a ser executado no mainframe, este nome deve
+      *                 estar associado aos comandos "JCL" na execução
+      *                 do programa (cartão DD que identifica o nome
+      *                 físico do arquivo associado com o nome lógico).
       *
+      * <disp-do-computador>: varia conforme o computador. Se no mainfra-
+      *                       me, usamos um formato composto por 3 seg-
+      *                       mentos separados por hífen.
       *
       ******************************************************************
-       DATA            DIVISION.
-       FILE            SECTION.
+       DATA DIVISION.
+      ******************************************************************
+
+      *----------------------------------------------------------------*
+       FILE SECTION.
+      *----------------------------------------------------------------*
+
+
+      *----------------------------------------------------------------*
        WORKING-STORAGE SECTION.
+      *----------------------------------------------------------------*
+
        01  WS-MENSAGEM PIC X(20) VALUE SPACES.
        01  WS-NOME     PIC X(20) VALUE SPACES.
+
+      *----------------------------------------------------------------*
+       LOCAL-STORAGE SECTION.
+      *----------------------------------------------------------------*
+
+
       ******************************************************************
-       PROCEDURE       DIVISION.
+       PROCEDURE DIVISION.
+      ******************************************************************
        0001-PRINCIPAL  SECTION.
             STOP RUN.
        END PROGRAM PROGEXEM.
